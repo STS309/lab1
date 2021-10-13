8e152817cead22b0242af9cd03ab905f453aae75
Pavel Sherstnev <sherst.pavel@yandex.ru>
2021-10-13 11:13:11 +0500

    change

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,12 @@
-alert("Hello, world!")
-console.log("123")
+nums = []
+for (i = 0; i < 10; i++)
+{
+    for (j = 0; j < 10; i++)
+    {
+        num1 = Math.random()
+        num2 = Maht.random()
+        nums.push([num1, num2])
+    }
+}
+
+
