5b0d2d93394263170fa73ebfb8f4e3473202d474
EstySD <sergeevdns@gmail.com>
2021-11-06 13:39:32 +0500

    Твой массив ранд. чисел:  c1[i]

--- JS/main.js
+++ JS/main.js
@@ -1,2 +1,23 @@
-alert("Hello, world!")
-console.log("123")
+function random()
+{
+    let x0, x1, k;
+    let c1=[];
+    k=Number(document.querySelector('#k').value);
+    x0=document.querySelector('#x0').value;
+    x1=document.querySelector('#x1').value;
+    n=document.querySelector('#n').value;
+    document.querySelector('#randout').innerHTML = "";
+    if (String(x0).length!=String(x1).length ) {
+        alert('Числа неоднозначные');
+        return 0;
+    }
+    for (i=0; i<n; i++) {
+        C= x0*x1;
+        c1[i]=Math.floor((C-Math.floor(C/Math.pow(10,k*3))*Math.pow(10, k*3))/Math.pow(10, k));
+        document.querySelector('#randout').innerHTML+=" "+String(c1[i]);
+        x0=x1;
+        x1=c1[i];
+        console.log(C, k, c1[i])
+    }  
+    //9044
+}
